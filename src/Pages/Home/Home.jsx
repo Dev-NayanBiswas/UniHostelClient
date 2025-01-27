edc1445146b1ec1a34734f949844e212c73a3b3b
Dev-NayanBiswas <dev.nayan.biswas@gmail.com>
2025-01-27 17:27:30 +0600

    range filter on price and indexing filter implimented

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,14 +1,49 @@
+import { useState } from "react"
 import Container from "../../Components/Container"
 import SubscriptionsContainer from "../../Components/SubscriptionCard/SubscriptionsContainer"
 import DynamicTitle from "../../Utilities/DynamicTitle"
 import CategoryMeals from "./MealByCategory/CategoryMeals"
 
 function Home(){
+  const [min, setMin] = useState(0)
+  const [max, setMax] = useState(0)
+
+
 
   return (
     <div>
       <DynamicTitle/>
-      Home
+      <section>
+      <section className="w-full mt-4 flex justify-center items-center gap-1">
+        <div className="flex justify-between">
+          <span className="py-1 text-xs text-center text-gray-bg/45 w-[61px] font-heading rounded-full bg-logo-yellow font-semibold">Min : {min}</span>
+        </div>
+        <input
+          type="range"
+          min="0"
+          max="20"
+          step="1"
+          value={min}
+          onChange={(e) => setMin(e.target.value)}
+          className="w-full bg-gray-600/25 h-[2px] rounded-full accent-logo-yellow appearance-none"
+        />
+      </section>
+      <section className="w-full mt-4 flex justify-center items-center gap-1">
+        
+        <input
+          type="range"
+          min="0"
+          max="20"
+          step="1"
+          value={max}
+          onChange={(e) => setMax(e.target.value)}
+          className="w-full bg-gray-600/25 h-[2px] rounded-full accent-logo-yellow appearance-none"
+        />
+        <div className="flex justify-between">
+          <span className="py-1 text-xs text-center text-gray-bg/45 w-[61px] font-heading rounded-full bg-logo-yellow font-semibold">Max : {max}</span>
+        </div>
+      </section>
+      </section>
       <Container>
       </Container>
 
--- src/Pages/Meals/Meals.jsx
+++ src/Pages/Meals/Meals.jsx
@@ -6,18 +6,40 @@ import MealCard from "../../Components/MealCard/MealCard";
 import HeadingTitle from "../../Components/HeadingTitle/HeadingTitle";
 import { useState } from "react";
 import InfiniteScroll from "react-infinite-scroll-component";
+import { HiMiniMagnifyingGlass } from "react-icons/hi2";
+import debounce from "lodash.debounce";
 
-async function fetchMeals(){
-  const response = await axios.get("/meals");
+async function fetchMeals({category, search, sort, min, max}){
+
+  const params = {
+    category: category !== 'all' ? category : undefined,
+    search: search || undefined,
+    sort: sort || undefined,
+    max: max || undefined,
+    min: min || undefined
+  }
+  const response = await axios.get("/meals/category/search/sort", {params});
   const result = await response.data;
+  console.log(result);
   return result;
 } 
 
 function Meals(){
   const [cardNumber, setCardNumber] = useState(8);
+  const [category, setCategory] = useState("all");
+  const [search, setSearch] = useState("");
+  const [sort, setSort] = useState("");
+  const [min, setMin] = useState(0)
+  const [max, setMax] = useState(0)
+
+  const handleSearchDebounced = debounce((value) => {
+    setSearch(value);
+  }, 1000);
+
   const {isLoading, data, isError, error} = useQuery({
-    queryKey:["meals"],
-    queryFn:fetchMeals,
+    queryKey:["meals", category, search, sort, min, max],
+    queryFn:()=>fetchMeals({category, search, sort, min , max}),
+    keepPreviousData:true
   })
 
   if(isLoading){
@@ -40,9 +62,98 @@ function Meals(){
     <>
     <DynamicTitle/>
     <HeadingTitle headingData={headingData} />
+    <section className="lg:w-10/12 w-full mx-auto">
+      <div className="flex flex-col justify-center w-full md:flex-row items-center gap-4">
+      {/* Category Dropdown */}
+      <div className="w-full">
+        <select
+          id="category"
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+          className="defaultInput text-logo-yellow font-heading !rounded-none !border-logo-yellow/45"
+        >
+          <option value="all">Category</option>
+          <option value="breakfast">Breakfast</option>
+          <option value="lunch">Lunch</option>
+          <option value="dinner">Dinner</option>
+        </select>
+      </div>
+
+      {/* Search Bar */}
+      <div className="w-full flex self-end items-center relative">
+        <label className="sr-only" htmlFor="search">
+          Search
+        </label>
+        <input
+          id="search"
+          type="text"
+          defaultValue={search}
+          onChange={(e)=>handleSearchDebounced(e.target.value)}
+          placeholder="Search . . ."
+          className="defaultInput text-logo-yellow font-heading !rounded-none !border-logo-yellow/45"
+        />
+        <HiMiniMagnifyingGlass className="absolute right-3 text-logo-yellow font-semibold" size={20} />
+      </div>
+
+      {/* Filter Dropdown */}
+      <div className="w-full">
+        <select
+          id="filter"
+          value={sort}
+          onChange={(e) => setSort(e.target.value)}
+          className="defaultInput text-logo-yellow font-heading !rounded-none !border-logo-yellow/45"
+        >
+          <option value="">Filter</option>
+          <option value="asc">Price: Low to High</option>
+          <option value="desc">Price: High to Low</option>
+          <option value="latest">Date: Latest</option>
+          <option value="oldest">Date: Oldest</option>
+        </select>
+      </div>
+    </div>
+    <div>
+    <section>
+      <section className="w-full mt-4 flex justify-center items-center gap-1">
+        <div className="flex justify-between">
+          <span className="py-1 text-xs text-center text-gray-bg/45 w-[61px] font-heading rounded-full bg-logo-yellow font-semibold">Min : {min}</span>
+        </div>
+        <input
+          type="range"
+          min="0"
+          max="20"
+          step="1"
+          value={min}
+          onChange={(e) => setMin(e.target.value)}
+          className="w-full bg-gray-600/25 h-[2px] rounded-full accent-logo-yellow appearance-none"
+        />
+      </section>
+      <section className="w-full mt-4 flex justify-center items-center gap-1">
+        
+        <input
+          type="range"
+          min="0"
+          max="20"
+          step="1"
+          value={max}
+          onChange={(e) => setMax(e.target.value)}
+          className="w-full bg-gray-600/25 h-[2px] rounded-full accent-logo-yellow appearance-none"
+        />
+        <div className="flex justify-between">
+          <span className="py-1 text-xs text-center text-gray-bg/45 w-[61px] font-heading rounded-full bg-logo-yellow font-semibold">Max : {max}</span>
+        </div>
+      </section>
+      </section>
+    </div>
+
+
+
+    </section>
+
+
+
     <section className="relative">
       <InfiniteScroll
-      dataLength={displayedCards.length}
+      dataLength={displayedCards?.length}
       next={handleCardNumber}
       hasMore={cardNumber < data?.result?.length}
       loader = {<span className="absolute -bottom-20 left-1/2 text-2xl font-semibold font-heading text-logo-yellow">Please wait . . .</span>}
